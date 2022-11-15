042fb045f0dea2941c55948cc56bcc4e8c26240f
FAYCALDOUIOU <faycal.douiou1999@gmail.com>
2022-11-15 12:09:29 +0100

    adding structure files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Logo from "./components/Logo.vue";
+import Info from "./components/Info.vue";
+</script>
+
+<template>
+  <main>
+    <Logo />
+    <Info />
+  </main>
+</template>
+
+<style scoped>
+</style>
